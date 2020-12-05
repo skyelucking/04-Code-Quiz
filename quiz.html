12e1c664b8ab67fc4851df174d0f493f94f9ea16
Skye Lucking <lucking.skye@gmail.com>
2020-12-05 14:15:15 -0700

    got office hours help + css up to snuff

---
+++ quiz.html
@@ -0,0 +1,56 @@
+var main = document.querySelector("main");
+var site1El = document.querySelector(".site1");
+var site2El = document.querySelector(".site2");
+var site3El = document.querySelector(".site3");
+
+var sites = [
+  "https://news.ycombinator.com/",
+  "https://twitter.com",
+  "https://instagram.com",
+  "https://google.com",
+  "https://reddit.com/r/webdev"
+];
+
+var siteDataList = [
+  {
+    element: site1El,
+    title: "Site 1",
+    url: "https://google.com",
+    imgSrc: "images/image_1.jpg",
+    imgAlt: "man working"
+  },
+  {
+    element: site2El,
+    title: "Site 2",
+    url: "https://twitter.com",
+    imgSrc: "images/image_2.jpg",
+    imgAlt: "group brainstorm"
+  },
+  {
+    element: site3El,
+    title: "Site 3",
+    url: "https://instagram.com",
+    imgSrc: "images/image_3.jpg",
+    imgAlt: "women working"
+  }
+];
+
+function renderSite(siteData) {
+  var titleElement = siteData.element.children[0];
+  var linkElement = siteData.element.children[1];
+  var imgElement = linkElement.children[0];
+
+  titleElement.textContent = siteData.title;
+  titleElement.setAttribute(
+    "style",
+    "text-decoration: underline; padding-left:10px; margin: 0"
+  );
+  linkElement.setAttribute("href", siteData.url);
+  imgElement.setAttribute("src", siteData.imgSrc);
+  imgElement.setAttribute("alt", siteData.imgAlt);
+  imgElement.setAttribute("style", "padding:10px;");
+}
+
+for (var i = 0; i < siteDataList.length; i++) {
+  renderSite(siteDataList[i]);
+}
--- script.js
+++ script.js
@@ -5,36 +5,43 @@ var totalSeconds = 0;
 var secondsElapsed = 0;
 var interval;
 
-// Turning on and Off Buttons
-var leadText = document.querySelector("#lead")
+// Quiz Body Elements
+var quizQuestionText = "test";
+var answerText1 = document.querySelector("#quizA1");
+var answerText2 = document.querySelector("#quizA2");
+var answerText3 = document.querySelector("#quizA3");
+var answerText4 = document.querySelector("#quizA4");
+var leadText = document.querySelector("#lead");
+var quizIntroText = document.querySelector("#quizIntro");
 
-
+//Timer and Count-Down
 playButton.addEventListener("click", startTimer);
 
 function startTimer() {
   setTime();
+  //Set Interval
   if (totalSeconds > 0) {
-        interval = setInterval(function() {
-        secondsElapsed++;
-        renderTime();
-      }, 1000);
-  } else {
-    secondsDisplay.textContent = secondsElapsed;
+    interval = setInterval(function () {
+      secondsElapsed++;
+      renderTime();
+      if (totalSeconds - secondsElapsed <= 0){
+        clearInterval(interval);
+      }
+    }, 1000);
     }
-  
 }
 function setTime() {
-   totalSeconds = 12;
+  totalSeconds = 4;
 }
 
 function renderTime() {
-  do {
-    secondsDisplay.textContent = getFormattedSeconds();
-    } while (secondsElapsed >= totalSeconds+1);
-  }
+  // do {
+  secondsDisplay.textContent = getFormattedSeconds();
+  //   } while (secondsElapsed >= totalSeconds + 1);
+}
 
 function getFormattedSeconds() {
-  var secondsLeft = (totalSeconds - secondsElapsed);
+  var secondsLeft = totalSeconds - secondsElapsed;
   var formattedSeconds;
   if (secondsLeft < 10) {
     formattedSeconds = "0" + secondsLeft;
@@ -42,8 +49,8 @@ function getFormattedSeconds() {
     formattedSeconds = secondsLeft;
   }
   return formattedSeconds;
-   }
- 
+}
 
+//Setting Up the Quiz Loop
 
- 
+var quizQuestionText = quiz1[question];
